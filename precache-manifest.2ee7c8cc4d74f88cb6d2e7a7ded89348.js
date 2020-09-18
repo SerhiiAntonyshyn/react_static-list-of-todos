921e3fb080c55a6458114253fb9538d70537758a
Serhii Antonyshyn <serginio4422@gmail.com>
2020-09-18 13:21:29 +0300

    Updates

---
+++ precache-manifest.2ee7c8cc4d74f88cb6d2e7a7ded89348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2013ae76c4df42c08b19ab6d0eea67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0cdca6ac46d6a055bb8",
+    "url": "./static/css/main.8d34ba0a.chunk.css"
+  },
+  {
+    "revision": "164620e3d804bc57b072",
+    "url": "./static/js/2.277daba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.277daba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0cdca6ac46d6a055bb8",
+    "url": "./static/js/main.0b98012d.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
